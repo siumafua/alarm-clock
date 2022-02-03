57d076c9dfb60b7e092ed44607d64bc3f76a6130
Siumafua Moala <siumafua.moala@gmail.com>
2022-02-04 10:08:26 +1300

    Alarm project first commmit

---
+++ alarm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from kivy.lang import Builder
+from kivy.metrics import dp
+
+from kivymd.app import MDApp
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.snackbar import Snackbar
+
+KV = '''
+MDBoxLayout:
+    orientation: "vertical"
+
+    MDToolbar:
+        title: "MDToolbar"
+        left_action_items: [["menu", lambda x: app.callback(x)]]
+        right_action_items: [["dots-vertical", lambda x: app.callback(x)]]
+
+    MDLabel:
+        text: "Content"
+        halign: "center"
+'''
+
+
+class Alarm(MDApp):
+    def build(self):
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": f"Item {i}",
+                "height": dp(56),
+                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
+             } for i in range(5)
+        ]
+        self.menu = MDDropdownMenu(
+            items=menu_items,
+            width_mult=4,
+        )
+        return Builder.load_string(KV)
+
+    def callback(self, button):
+        self.menu.caller = button
+        self.menu.open()
+
+    def menu_callback(self, text_item):
+        self.menu.dismiss()
+        Snackbar(text=text_item).open()
+
+
+Test().run()
